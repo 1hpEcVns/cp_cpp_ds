70228c7e6091612d43defde9d76c2cf655cafc6c
1hpecvns <ihpecvns@gmail.com>
2025-05-12 03:07:54 +0800

    更新.clangd配置，修改C++标准为C++17并添加多个编译选项；重构E_Holes.cpp，优化输入输出函数，使用更高效的字符读取和写入方式，增加了代码可读性和性能。

--- E_Holes.cpp
+++ E_Holes.cpp
@@ -1,24 +1,60 @@
-#include <iostream>
-using I = long;
-I n, x, y, t[1 << 20], p;
-void A(I x, I y) {
-  while (x <= n)
-    t[x] += y, x += x & -x;
+#include <cstdio>
+
+const int N_io = 1e6 + 10;
+
+char *p1, *p2, buf[N_io];                                            // NOLINT
+[[nodiscard]] inline char nc() noexcept {                         // NOLINT
+  return (p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, N_io, stdin), // NOLINT
+                       p1 == p2)                                  // NOLINT
+              ? EOF
+              : *p1++); // NOLINT
 }
-I Q(I x) {
-  for (y = 0; x; x ^= x & -x)
-    y += t[x];
-
-  return y;
+int read() {                   // NOLINT
+  int x = 0, f = 1;            // NOLINT
+  char ch = nc();              // NOLINT
+  while (ch < 48 || ch > 57) { // NOLINT
+    if (ch == '-')             // NOLINT
+      f = -1;                  // NOLINT
+    ch = nc();                 // NOLINT
+  } // NOLINT
+  while (ch >= 48 && ch <= 57)                      // NOLINT
+    x = (x << 3) + (x << 1) + (ch ^ 48), ch = nc(); // NOLINT
+  return x * f;                                     // NOLINT
 }
-int main() {
-  std::cout << '六';
-  
-  std::cin >> n >> y;
 
-  while (n - x++)
-    std::cin >> y, A(x, y);
+char obuf[N_io], *p3 = obuf; // NOLINT
+inline void putchar(char x) { // NOLINT
+  (p3 - obuf < N_io)         // NOLINT
+      ? (*p3++ = x)          // NOLINT
+      : (fwrite(obuf, p3 - obuf, 1, stdout), p3 = obuf, *p3++ = x); // NOLINT
+}
+inline void write(int x) { // NOLINT
+  if (!x) {                // NOLINT
+    putchar('0');          // NOLINT
+    return;                // NOLINT
+  } // NOLINT
+  int len = 0, k1 = x, c[40];          // NOLINT
+  if (k1 < 0)                          // NOLINT
+    k1 = -k1, putchar('-');            // NOLINT
+  while (k1)                           // NOLINT
+    c[len++] = k1 % 10 ^ 48, k1 /= 10; // NOLINT
+  while (len--)                        // NOLINT
+    putchar(c[len]);                   // NOLINT
+}
 
-  while (std::cin >> p >> x >> y)
-    p & 1 ? A(x, y), 1 : std::cout << Q(y) - Q(x - 1) << '\n';
+//以上为快读快写
+
+#include <vector>
+#include <array>
+
+using namespace std;
+
+auto main() -> int {
+  auto n = read(), m = read();
+
+  auto a=vector(n,array{0,0,0})
+
+  while (m--) {
+    
+  }
 }
